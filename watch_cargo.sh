619b85e4a92b4e110ba6cfea09ad7f0a55a00a53
vaysseb <bvayssegm@gmail.com>
2017-12-09 20:04:52 +0000

    Recompilation now triggered by inotify

--- watch_cargo.sh
+++ watch_cargo.sh
@@ -3,8 +3,6 @@ CMD="clear ; cargo run 2>&1 | head -n 40"
 sh -c "$CMD"
 
 while true
-do 
-    touch ./lastwatch
-    sleep 3
-    find src -cnewer ./lastwatch -exec sh -c "$CMD" {} +
+do
+    inotifywait --recursive --event modify ./src 2>/dev/null && sh -c "$CMD"
 done
